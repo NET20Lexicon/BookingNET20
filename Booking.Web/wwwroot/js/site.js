6051ac5cdded0e61169c6420d8eaaa0bf6e35cf1
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2020-11-09 13:36:36 +0100

    Jquery Ajax

--- Booking.Web/wwwroot/js/site.js
+++ Booking.Web/wwwroot/js/site.js
@@ -5,22 +5,40 @@
 
 // Write your JavaScript code.
 
-document.querySelector('#ajax').addEventListener('click', callBackEnd);
+//document.querySelector('#ajax').addEventListener('click', callBackEnd);
 let p = document.querySelector('#createajax');
 
-async function callBackEnd() {
-    fetch('https://localhost:5001/GymClasses/fetch',
-        {
-            method: 'GET',
-        })
-        .then(res => res.text())
-        .then(data => {
-            p.innerHTML = data;
-        })
-        .catch(err => console.log(err));
-};
+//async function callBackEnd() {
+//    fetch('https://localhost:5001/GymClasses/fetch',
+//        {
+//            method: 'GET',
+//        })
+//        .then(res => res.text())
+//        .then(data => {
+//            p.innerHTML = data;
+//        })
+//        .catch(err => console.log(err));
+//};
 
+$('#ajax').click(function () {
+    $.ajax({
+        url: 'https://localhost:5001/GymClasses/fetch',
+        type: 'GET',
+        success: success,
+        failure: failure
+    });
+});
 
+function success(response) {
+    if (200 == response.status) {
+        console.log("Success")
+    }
+    p.innerHTML = response;
+}
+
+function failure(response) {
+    console.log('Error');
+}
 
 
 $(document).ready(function () {
